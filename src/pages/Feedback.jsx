58cd8cbbf0bc32154ad748ba7b2d799b2caa64eb
Renata <renatateixeira.est@gmail.com>
2021-10-26 17:08:01 -0300

    Requisito 15

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Feedback extends Component {
   render() {
     return (
       <div>
         <h1 data-testid="feedback-text">Feedback</h1>
+        <Link to="/">
+          <button type="button" data-testid="btn-play-again">
+            Jogar novamente
+          </button>
+        </Link>
       </div>
     );
   }
