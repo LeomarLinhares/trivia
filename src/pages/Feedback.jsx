180bd2b36382e9da9d90df221dc8831344319677
Renata <renatateixeira.est@gmail.com>
2021-10-26 17:17:20 -0300

    Requisito 15

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -5,7 +5,9 @@ export default class Feedback extends Component {
   render() {
     return (
       <div>
-        <h1 data-testid="feedback-text">Feedback</h1>
+        <h1 data-testid="feedback-text">
+          Feedback
+        </h1>
         <Link to="/">
           <button type="button" data-testid="btn-play-again">
             Jogar novamente
